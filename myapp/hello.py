fb5f27c1d81c9ca6878aa9a009630e9ca8c2bcb6
Mahmoud Nobani <56834783+MahmoudNobani@users.noreply.github.com>
2023-11-05 12:56:21 +0200

    Update hello.py

--- myapp/hello.py
+++ myapp/hello.py
@@ -1,7 +1,7 @@
 import fire
 
-def hello(name="World"):
-  return "Hello %s!" % name
+def hello(name="Worlds"):
+  return "Hello %s! and T1 fighting" % name
 
 if __name__ == '__main__':
   fire.Fire(hello)
